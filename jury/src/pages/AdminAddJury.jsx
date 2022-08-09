ad1b3cebcbfb79c48f9ad27d2a364b9eff375dc8
Petr <106156164+PetrKamennov@users.noreply.github.com>
2022-08-09 16:50:50 +0400

    specal for alexis 2

--- jury/src/pages/AdminAddJury.jsx
+++ jury/src/pages/AdminAddJury.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { useState } from "react";
 
 import "../components/AdminAddJury/AdminAddJury.css";
+import AdminAddJuryModal from "../components/AdminAddJury/AminAddJuryModal";
 
 const AdminAddJury = () => {
+
+    const [modalActive, setModalActive] = useState(true);
+
     return (
+        <>
         <section className="AddJurySection">
             <h1>Добавление члена жури</h1>
             <div className="AddJurySection__pullJury">
@@ -12,9 +18,16 @@ const AdminAddJury = () => {
                 <div className="jury"></div>
                 <div className="jury"></div>
                 <div className="jury"></div>
+                <div className="jury"></div>
+                <div className="jury"></div>
+                <div className="jury"></div>
+                <div className="jury"></div>
+                <div className="jury"></div>
             </div>
             <button>Зарегистрировать нового члена жури</button>
         </section>
+        <AdminAddJuryModal active={modalActive} setActive={setModalActive}/>
+        </>
     )
 }
 
