4e4a3c41db4d605ffa187fd795a05c30e560df30
Petr <106156164+PetrKamennov@users.noreply.github.com>
2022-08-09 15:27:58 +0400

    special for Alexis

--- jury/src/pages/AdminAddJury.jsx
+++ jury/src/pages/AdminAddJury.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 
+import "../components/AdminAddJury/AdminAddJury.css";
+
 const AdminAddJury = () => {
-
-
-    return (0)
+    return (
+        <section className="AddJurySection">
+            <h1>Добавление члена жури</h1>
+            <div className="AddJurySection__pullJury">
+                <div className="jury"></div>
+                <div className="jury"></div>
+                <div className="jury"></div>
+                <div className="jury"></div>
+                <div className="jury"></div>
+            </div>
+            <button>Зарегистрировать нового члена жури</button>
+        </section>
+    )
 }
 
 export default AdminAddJury
