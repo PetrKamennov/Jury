4cd238ed77713a838173f16890845b443c6498fa
Petr <106156164+PetrKamennov@users.noreply.github.com>
2022-08-09 14:57:11 +0400

    Страницы админа

---
+++ jury/src/pages/AdminAddJury.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const AdminAddJury = () => {
+
+
+    return (0)
+}
+
+export default AdminAddJury
---
+++ jury/src/pages/AdminEditJury.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const AdminEditJury = () => {
+
+
+    return (0)
+}
+
+export default AdminEditJury
