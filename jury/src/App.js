e86b32202a2ecdb589b159045e5d73a17204d7c4
Petr <106156164+PetrKamennov@users.noreply.github.com>
2022-08-12 19:17:08 +0400

    Информаци доделана(без бэка)

--- jury/src/App.js
+++ jury/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Route, Routes } from 'react-router-dom';
 import AdminEditJury from './pages/AdminEditJury';
+import AdminEditMeet from './pages/AdminEditMeet';
 import AdminAddJury from './pages/AdminAddJury';
 import AdminEditProject from './pages/AdminEditProject';
 import Navbar from './components/navbar/Navbar';
@@ -13,11 +14,12 @@ import PasswordRecovery from './pages/PasswordRecovery';
 function App() {
   return (
     <>
-      <Navbar/>
+      {/* <Navbar/> */}
       <Routes>
         <Route path='/AdminEditProject' element={<AdminEditProject/>}/>
         <Route path='/AdminEditJury' element={<AdminEditJury />} />
         <Route path='/AdminAddJury' element={<AdminAddJury />} />
+        <Route path='/AdminEditMeet' element={<AdminEditMeet />} />
       </Routes>
     </>
   );
--- jury/src/pages/AdminEditMeet.jsx
+++ jury/src/pages/AdminEditMeet.jsx
@@ -1,9 +1,60 @@
 import React from "react";
+import { useState } from "react";
+
+import "../components/AdminEditMeet/AdminEditMeet.css";
+import EditCriteria from "../components/AdminEditMeet/EditCriteria";
+import Criteria from "../components/AdminEditMeet/Сriteria";
 
 const AdminEditMeet = () => {
 
+    const [criterias, setcriteria] = useState([
+        { id: 1, criterianame: 'Проект хуйни', description: 'Долбаеееб' },
+        { id: 2, criterianame: 'Проект хуйни', description: 'Долбаеееб' },
+        { id: 3, criterianame: 'Проект хуйни', description: 'Долбаеееб' },
+        { id: 4, criterianame: 'Проект хуйни', description: 'Долбаеееб' },
+    ])
 
-    return (0)
+    const createcriteria = (newCriteria) => {
+        setcriteria([...criterias, newCriteria])
+    }
+
+    const removeProject = (criteria) => {
+        setcriteria(criterias.filter(p => p.id !== criteria.id))
+    }
+
+    return (
+        <>
+            
+            <section className="AdminEditMeet">
+                <div className="AdminEditMeet__firstBlock">
+                    <div className="AdminEditMeet__firstBlock-container">
+                        <div className="AdminEditMeet__firstBlock__left">
+                            <input placeholder="Наименование проекта" type="text" />
+                            <input placeholder="Дата и время проведения" type="text" />
+                        </div>
+                        <div className="AdminEditMeet__firstBlock__right">
+                            <button>Удалить мероприятие</button>
+                            <button>Добавить члена жюри</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="AdminEditMeet__secondBlock">
+                    <div className="AdminEditMeet__secondBlock__header">
+                        <h1>Критерии оценивания</h1>
+                    </div>
+                    <div className="AdminEditMeet__secondBlock-container">
+                        <div className="AdminEditMeet__secondBlock__criteriaPull">
+                            {criterias.map((criterias, index) =>
+                                <Criteria remove={removeProject} number={index + 1} criteria={criterias} key={criterias.id} />
+                            )}
+                        </div>
+                        <EditCriteria create={createcriteria}/>
+                    </div>
+                </div>
+                <button>Сохранить</button>
+            </section>
+        </>
+    )
 }
 
 export default AdminEditMeet
