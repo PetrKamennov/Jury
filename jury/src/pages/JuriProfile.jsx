a19f0aeac200b56bdadbe499a6df303253caa108
LXMinus <108346585+LXMinus@users.noreply.github.com>
2022-08-12 12:05:24 +0400

    Редизайн профиля

--- jury/src/pages/JuriProfile.jsx
+++ jury/src/pages/JuriProfile.jsx
@@ -4,26 +4,47 @@ import {Link} from "react-router-dom";
 const JuriProfile = () => {
     return (
         <div className="JuryProfile">
-            <div className="first_block_profile">
-                <div className="your_photo"></div>
-                <div className="inputs_FIO">
-                    <input placeholder="Имя" className="data_inp name_inp"/>
-                    <input placeholder="Фамилия" className="data_inp surname_inp"/>
-                    <input placeholder="Отчество (при наличии)" className="data_inp patronymic_inp"/>
+            <div className="main_block_profile">
+                <div className="block_of_profile first_block_prof">
+                    <div className="your_photo"></div>
+                    <div className="butts_jury_block photo_butts">
+                        <Link to="" className="butt_edit_profile give_photo">Добавить фото</Link>
+                        <Link to="" className="butt_edit_profile send_photo">Отправить фото</Link>
+                    </div>
                 </div>
-            </div>
-            <div className="block_jury_profile_butts">
-                <div className="butts_jury_block photo_butts">
-                    <Link to="" className="butt_jury_profile give_photo">Добавить фото</Link>
-                    <Link to="" className="butt_jury_profile send_photo">Отправить фото</Link>
+                <div className="block_of_profile scnd_block_prof">
+                    <div className="inputs_FIO">
+                        <div>
+                            <h4 className="placeholder_for_input">Имя</h4>
+                            <input  className="data_inp name_inp"/>
+                        </div>
+                        <div>
+                            <h4 className="placeholder_for_input">Фамилия</h4>
+                            <input className="data_inp surname_inp"/>
+                        </div>
+                        <div>
+                            <h4 className="placeholder_for_input">Отчество (при наличии)</h4>
+                            <input className="data_inp patronymic_inp"/>
+                        </div>
+                    </div>
+                    <div className="butts_jury_block data_butts">
+                        <Link to="" className="butt_jury_profile change_password">Сменить пароль</Link>
+                        <Link to="" className="butt_jury_profile change_login">Сменить логин</Link>
+                    </div>
                 </div>
-                <div className="butts_jury_block data_butts">
-                    <Link to="" className="butt_jury_profile change_password">Сменить пароль</Link>
-                    <Link to="" className="butt_jury_profile change_login">Сменить логин</Link>
+                <div className="block_of_profile third_block_prof">
+                    <div>
+                        <h4 className="placeholder_for_input">Должность</h4>
+                        <input  className="data_inp post_inp"/>
+                    </div>
+                    <div>
+                        <h4 className="placeholder_for_input">Логин</h4>
+                        <input className="data_inp login_inp"/>
+                    </div>
                 </div>
             </div>
             <div className="save_your_profile">
-                <Link to="" className="butt_jury_profile save_profile">Подтвердить</Link>
+                <Link to="" className="save_profile">Сохранить</Link>
             </div>
         </div>
     );
--- jury/src/pages/JuryMeet.jsx
+++ jury/src/pages/JuryMeet.jsx
@@ -8,7 +8,7 @@ const JuryMeet = () => {
     return (
         <div className="JuryMeet">
             <div className="buttons_jury_meet">
-                <Link to=""  className="jury_meet_button first_button">текущие мероприятия</Link>
+                <Link to=""  className="jury_meet_button first_button">Текущие мероприятия</Link>
                 <Link to="/aboba" className="jury_meet_button scnd_button">История</Link>
                 <Link to="/profile" className="jury_meet_button third_button">Профиль</Link>
             </div>
