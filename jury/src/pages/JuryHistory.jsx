a5a32daa0659c345b5666e7536175ed247af14d2
LXMinus <108346585+LXMinus@users.noreply.github.com>
2022-08-17 11:58:40 +0400

    history
    ready

--- jury/src/pages/JuryHistory.jsx
+++ jury/src/pages/JuryHistory.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import "../components/JuryMeet/jury_history.css"
+import {useState} from "react";
+import EventProjectJury from "../components/JuryMeet/EventProjectJury";
+import EventProjectHistory from "../components/JuryMeet/EventProjectHistory";
 const JuryHistory = () => {
+
+    const [projects, setprojects] = useState([
+        { id: 1, projectname: 'Мероприятие', date: '10.08.2001' },
+        { id: 2, projectname: 'Мероприятие', date: '10.08.2001'  },
+        { id: 3, projectname: 'Мероприятие', date: '10.08.2001'  },
+        { id: 4, projectname: 'Мероприятие', date: '10.08.2001'  },
+    ])
+
     return (
         <div className="JuriHistory">
-            <div className="central_part_selectors">
-
-
-            </div>
+            <section className="Jury_History_select">
+                <div className="JuryHistory__ProjektPull">
+                    {projects.map((projects, index) =>
+                        <EventProjectHistory number={index + 1} project={projects} key={projects.id} />
+                    )}
+                </div>
+            </section>
         </div>
     );
 };
