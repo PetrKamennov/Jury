e40b4a705c4064102087a4914693afb539aefe45
Petr <106156164+PetrKamennov@users.noreply.github.com>
2022-10-06 13:13:37 +0400

    Revert "Revert "Переход на новый бэк""
    This reverts commit 039ce8cda619614fc873e79cb380ea3d13a16ca5.

---
+++ jury/src/pages/EmailChange.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+import "../components/Authorization/EmailChange.css"
+import { Link } from "react-router-dom"
+
+
+const EmailChange = () => {
+
+
+    return (
+        <>
+            <section className="email_change">
+                <div className="email_change__block">
+                    <div className="email_change__block_content">
+                        <h1>Сброс пароля</h1>
+                        <p>Заявка на восстановление пароля отправлена.</p>
+                        <Link className="goto__button_email_change" to="/">Авторизироваться</Link>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default EmailChange
